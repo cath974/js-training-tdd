bd8f693d87c452dcca9a3be6001870803b502295
Catherine <catblecon@gmail.com>
2020-04-02 13:22:11 +0100

    Solve yell

--- exercises/yell.js
+++ exercises/yell.js
@@ -5,11 +5,23 @@
  * and return the same string in upper case
  *
  */
+const yell = (str) => {
+    return str.toUpperCase()
+}
 
 // Your code:
 
 //* Begin of tests
 const assert = require('assert');
+//* Begin of tests
 
-assert.fail('You must write your own tests');
+assert.strictEqual(typeof yell, 'function');
+assert.strictEqual(yell.length, 1);
+assert.strictEqual(yell('hello'), 'HELLO');
+assert.strictEqual(yell('hEllo'), 'HELLO');
+
+// End of tests */
+// assert.fail('You must write your own tests');
+
+
 // End of tests */
